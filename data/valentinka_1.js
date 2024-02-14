7f0661ad0136a6a63dd957c81e70ebe4641464b1
geomaps <107353795+gemaps@users.noreply.github.com>
2024-02-14 10:36:00 +0300

    Add files via upload

---
+++ data/valentinka_1.js
@@ -0,0 +1,13 @@
+var json_valentinka_1 = {
+"type": "FeatureCollection",
+"name": "valentinka_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "popup": "Приближайся к сердцу! С днем всех влюбленных!" }, "geometry": { "type": "Point", "coordinates": [ 37.13972242016608, 55.052117352584425 ] } },
+{ "type": "Feature", "properties": { "popup": "А это в Европе..." }, "geometry": { "type": "Point", "coordinates": [ 15.383683095188216, 43.978382310439571 ] } },
+{ "type": "Feature", "properties": { "popup": "Арабские дубаи" }, "geometry": { "type": "Point", "coordinates": [ 55.404549366860685, 24.838418537893382 ] } },
+{ "type": "Feature", "properties": { "popup": "Эфрика е-маё, короче это валентинки" }, "geometry": { "type": "Point", "coordinates": [ 15.565447333211935, -23.650745372158571 ] } },
+{ "type": "Feature", "properties": { "popup": "КЭНЭДА" }, "geometry": { "type": "Point", "coordinates": [ -76.898137289502273, 45.019148474244716 ] } },
+{ "type": "Feature", "properties": { "popup": "юэ сэй Харт" }, "geometry": { "type": "Point", "coordinates": [ -81.901714515770678, 41.303934037753592 ] } }
+]
+}
